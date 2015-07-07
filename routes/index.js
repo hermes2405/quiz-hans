6a3b28285477f7b06cf749059812e45fbcebc6db
Antonio Ropero Muela <antonio.ropero@yahoo.es>
2015-07-07 19:44:57 +0200

    Primera página y favicon

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,13 @@
-/* GET home page. */
+/* GET home page.
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
-};
+}; */
+var express = require('express');
+var router = express.Router();
+
+/*GET home page. */
+router.get('/',function(req, res){
+  res.render('index', {title:'Quiz'});
+});
+
+module.exports = router;
