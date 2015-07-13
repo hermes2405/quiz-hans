e8ae16386f1854e9523859a04be06832362be461
unknown <i-hmontenegro@TISIS-INFOTE-02.central.cotas.com>
2015-07-13 16:38:59 -0400

    .

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,7 @@ router.get('/author', function(req, res) {
 		profesion : "Ing. de Sistemas",
 		foto      : "image/foto.jpg"
 	}
-  	res.render('index', datos);
+  	res.render('author', datos);
 });
 
 router.get('/quizes/question', quizController.question);
