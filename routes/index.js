7aff37a3a4dff1cbfd051c1a28a02f521d9cd591
unknown <i-hmontenegro@TISIS-INFOTE-02.central.cotas.com>
2015-07-13 16:29:49 -0400

    Cambios en rama creditos terminados

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,16 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+router.get('/', function(req, res) {
+	var datos = {
+		nombre    : "Alberto",
+		apellidos : "Montenegro Sejas",
+		profesion : "Ing. de Sistemas",
+		foto      : "image/foto.jpg"
+	}
+  	res.render('index', datos);
+});
+
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 
